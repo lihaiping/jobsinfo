e05d735ebd229438350e0d281694727ff08104aa
李海萍 <lihaiping.msg@gmail.com>
2014-03-02 16:52:00 +0800

    modified controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 JobsInfo::Application.routes.draw do
 
-  resources :weixins
+  get  'weixins' => 'weixins#index'
+  post 'weixins' => 'weixins#create'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
