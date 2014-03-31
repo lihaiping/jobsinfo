3853cad11ef1b1fb46d5fc089a34f73ab85ca5ef
李海萍 <lihaiping.msg@gmail.com>
2014-03-31 22:40:36 +0800

    reset comment

--- app/controllers/mobile/subscriptions_controller.rb
+++ app/controllers/mobile/subscriptions_controller.rb
@@ -6,23 +6,23 @@ class Mobile::SubscriptionsController < Mobile::ApplicationController
 
 	def setting
 		# For test
-		@user = User.first
-		@jobs = @user.jobs
+		# @user = User.first
+		# @jobs = @user.jobs
 
-		# if STATE == params[:state]
-		# 	if params[:code].present?
-		# 		oauth = Weixin::OAuth.new(@config.app_id, @config.app_secret)
-		# 		json = oauth.exchange_token(params[:code])
+		if STATE == params[:state]
+			if params[:code].present?
+				oauth = Weixin::OAuth.new(@config.app_id, @config.app_secret)
+				json = oauth.exchange_token(params[:code])
 
-		# 		@user = User.find_by(openid: json["openid"])
-		# 		@jobs = @user.jobs
-		# 	else
-		# 		redirect_to mobile_unauthorize_path
-		# 	end
-		# else
-		# 	url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{@config.app_id}&redirect_uri=#{request.original_url}&response_type=code&scope=snsapi_base&state=#{STATE}#wechat_redirect"
-  #       		redirect_to url
-		# end
+				@user = User.find_by(openid: json["openid"])
+				@jobs = @user.jobs
+			else
+				redirect_to mobile_unauthorize_path
+			end
+		else
+			url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{@config.app_id}&redirect_uri=#{request.original_url}&response_type=code&scope=snsapi_base&state=#{STATE}#wechat_redirect"
+        		redirect_to url
+		end
 	end
 
 	def set
