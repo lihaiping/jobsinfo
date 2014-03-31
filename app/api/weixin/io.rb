b6d999cad7f25ad12d3ae59d0dc6f4bb3bde8122
李海萍 <lihaiping.msg@gmail.com>
2014-03-31 20:57:56 +0800

    reset oauth authorize path

--- app/api/weixin/io.rb
+++ app/api/weixin/io.rb
@@ -158,7 +158,7 @@ module Weixin
 			end
 
 			def jobs_subscription
-				content = @config.jobs_subscription + '<a href="www.google.com">' + '点此设置'+ '</a>'
+				content = @config.jobs_subscription + '<%= link_to "点此设置", mobile_subscriptions_path %>'
 				Weixin.text_msg(@msg[:to_user], @msg[:from_user], content)
 			end
 
