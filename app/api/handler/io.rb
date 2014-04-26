0f7bef01ebc918787f979aa321a551bb3665c47f
李海萍 <lihaiping.msg@gmail.com>
2014-04-26 23:27:44 +0800

    modified subscription logic

--- app/api/handler/io.rb
+++ app/api/handler/io.rb
@@ -82,14 +82,13 @@ module Handler
 
 			def subscribe
 				user_info = @client.user.info(@msg[:from_user])
-				if 1 == user_info['subscribe']
+				if 0 == user_info['subscribe']
 					user = User.find_or_initialize_by(openid: @msg[:from_user]) do |user|
 						user.nickname = user_info['nickname']
 						user.area = user_info['province'] + user_info['city']
 						user.subscribe = 1
 					end
 					user.save
-					user.update_attributes(subscribe: 1) unless user.new_record?
 					Weixin.text_msg(@msg[:to_user], @msg[:from_user], @config.subscribe + @config.help)
 				end
 			end
