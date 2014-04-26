cb333f05678232b7ca4600950e16766fa5ae50ce
李海萍 <lihaiping.msg@gmail.com>
2014-04-26 23:48:37 +0800

    changed if condition

--- app/api/handler/io.rb
+++ app/api/handler/io.rb
@@ -82,7 +82,7 @@ module Handler
 
 			def subscribe
 				user_info = @client.user.info(@msg[:from_user])
-				if 0 == user_info['subscribe']
+				if 1 == user_info['subscribe']
 					user = User.find_or_initialize_by(openid: @msg[:from_user]) do |user|
 						user.nickname = user_info['nickname']
 						user.area = user_info['province'] + user_info['city']
